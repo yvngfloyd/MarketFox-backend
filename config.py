a3042cd90953f87b31957ea9c23348813cd2c05e
yvngfloyd <125301624+yvngfloyd@users.noreply.github.com>
2025-12-27 17:37:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
-if not BOT_TOKEN:
-    raise RuntimeError("BOT_TOKEN not set")
+if not BOT_TOKEN or not WEBHOOK_URL:
+    raise RuntimeError("BOT_TOKEN or WEBHOOK_URL not set")
